5bdd3563fdbc36c815242be4e6a2f0e0bdf95b59
MarchikM <m.chk@ya.ru>
2016-07-07 16:57:17 +0300

    add exercise one

---
+++ src/Exercise1/Test.java
@@ -0,0 +1,32 @@
+package Exercise1;
+
+/**
+ * За один цикл вычислить значение функции F(n) = 1!*2!*3!...*n!., где n! = 1*2*3*…*n. /без рекурсии/
+ */
+
+public class Test {
+
+    public int factorial(int num) {
+
+        int[] arr = new int[num];
+        int result = 1;
+        int finalResult = 1;
+
+        for (int i = 1; i <= num; i++) {
+            result = result * i;
+            arr[i-1] = result;
+            finalResult = finalResult * arr[i-1];
+        }
+
+        return finalResult;
+    }
+
+    public static void main(String[] args) {
+        Test e = new Test();
+        int result = e.factorial(4);
+
+        System.out.println(result);
+    }
+
+
+}
